c32571570d7f7bb5c40c5cac32e0e8fcc2dcfaa0
AlbinHellman <steelalbin@gmail.com>
2021-04-26 22:41:16 +0200

    JavaScript3 Inlämningsuppgift1

--- Inlämningsuppgift-1/uppgift1/src/App.js
+++ Inlämningsuppgift-1/uppgift1/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './komponenter/navigation/Navbar';
 import News from './views/News';
 import PostDetails from './views/PostDetails';
+import Login from './views/Login';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
      <div className="container">
        <Switch>
          <Route exact path="/" component={News} />
-
+         
+         <Route exact path="/login" component={Login} />
          <Route exact path="/posts/:id" component={PostDetails} />
        </Switch>
      </div>
---
+++ Inlämningsuppgift-1/uppgift1/src/views/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className="loggain">
+            <h1>LOGGA IN</h1>
+            <button>Logga In</button>
+            
+        </div>
+    )
+}
+
+export default Login
