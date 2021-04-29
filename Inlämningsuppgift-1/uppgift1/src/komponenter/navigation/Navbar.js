ac710cee848cc9225fe9a7ab998066ebe3f9062e
AlbinHellman <steelalbin@gmail.com>
2021-04-29 03:08:10 +0200

    JavaScript3 Inlämningsuppgift1

--- Inlämningsuppgift-1/uppgift1/src/komponenter/navigation/Navbar.js
+++ Inlämningsuppgift-1/uppgift1/src/komponenter/navigation/Navbar.js
@@ -10,16 +10,28 @@ const Navbar = () => {
 
 
     return (
+        
         <nav className="navbar">
             <div className="container">
                 <NavLink exact to="/" className="navbar-brand">LOGGA</NavLink>
                 <ul className="nav-links">
                     <li><NavLink exact to="/" className="nav-link" activeClassName="active-link">Nyheter</NavLink></li>
                    
+                    <li><a href="#"><i class="fas fa-shopping-cart"></i></a>
+                    <ul>
+                        <li><a href="#">Kundvagn</a></li>
+                        </ul>
+                        </li>
+                        
+ 
+                        
+                   
+                   
                    
                   {/*  <li><NavLink exact to="/login" className="nav-link" activeClassName="active-link">Logga In</NavLink></li> */}
                   {
                       isAuth ? <li><NavLink exact to="/create" className="nav-link" activeClassName="active-link">Skapa Ny Produkt</NavLink></li>
+
                      : <li><NavLink exact to="/login" className="nav-link" activeClassName="active-link">Logga In</NavLink></li>
                   }
                     </ul>
@@ -27,6 +39,7 @@ const Navbar = () => {
                     </nav>
     )
 }
+
 
 export default Navbar
 
